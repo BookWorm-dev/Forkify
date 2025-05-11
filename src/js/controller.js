f2d0e58539fdd156712e70b30c5df267fe87903a
Kristina <kgritsenko.2017@gmail.com>
2025-05-11 16:47:01 +0300

    Rendering the Recipe, Refactoring for MVC, Helpers and Configuration Files

---
+++ src/js/controller.js
@@ -0,0 +1,35 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+
+const recipeContainer = document.querySelector('.recipe');
+
+// NEW API URL (instead of the one shown in the video)
+// https://forkify-api.jonas.io
+
+///////////////////////////////////////
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    //1 Loading recipe
+    await model.loadRecipe(id);
+    const { recipe } = model.state;
+    //2 Rendering recipe
+
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    alert(err);
+  }
+};
+
+['hashchange', 'load'].forEach((ev) => window.addEventListener(ev, controlRecipes));
+
+// window.addEventListener('hashchange', controlRecipes);
+// window.addEventListener('load', controlRecipes);
