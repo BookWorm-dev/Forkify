a9a0e980b8ceee032bdca05aa2d17cc2a4ccd5d7
Kristina <kgritsenko.2017@gmail.com>
2025-05-11 20:09:22 +0300

    add Publisher-Subscriber Pattern,Implementing Error

--- src/js/controller.js
+++ src/js/controller.js
@@ -20,16 +20,16 @@ const controlRecipes = async function () {
 
     //1 Loading recipe
     await model.loadRecipe(id);
-    const { recipe } = model.state;
+    // const { recipe } = model.state;
     //2 Rendering recipe
 
     recipeView.render(model.state.recipe);
   } catch (err) {
-    alert(err);
+    recipeView.renderError();
   }
 };
 
-['hashchange', 'load'].forEach((ev) => window.addEventListener(ev, controlRecipes));
-
-// window.addEventListener('hashchange', controlRecipes);
-// window.addEventListener('load', controlRecipes);
+const init = function () {
+  recipeView.addHandleRender(controlRecipes);
+};
+init();
